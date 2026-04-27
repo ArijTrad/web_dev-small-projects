41e6608a62680c2300b17ec0a9fe8677e09fd560
ArijTrad <astra.arij@gmail.com>
2026-04-27 14:17:12 +0300

    ,,,

---
+++ js_train/todoapp/readme.txt
@@ -0,0 +1,18 @@
+HTML structure	✅
+CSS layout (flexbox, positioning)	✅
+CSS units (vh, %, clamp)	✅
+DOM manipulation	✅
+Event listeners (click, keypress)	✅
+preventDefault()	✅
+Template literals	✅
+Creating elements	✅
+Debugging	✅
+Problem solving	✅
+
+
+
+wanna use classList  for sth like this so it's like you make css for many classes liek tailwind and just change the class name based on what you need or event listener like clicking on a theme toggle button
+
+wanna make the list put the checked item down to the end of teh list
+
+
--- js_train/todoapp/script.js
+++ js_train/todoapp/script.js
@@ -1,47 +1,54 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const taskInput = document.getElementById('task-input');
-    const addTaskBtn= document.getElementById('add-task-btn');
-    const taskList = document.getElementById('task-list');
-    const emptyImg = document.querySelector('.empty-image');
+    const $taskInput = document.getElementById('task-input');
+    const $addTaskBtn= document.getElementById('add-task-btn');
+    const $taskList = document.getElementById('task-list');
+    const $emptyImg = document.querySelector('.empty-image');
 
+
+
+    /*function to toggle the visibility of the empty image*/
     const toggleEmptyState=()=>{
-        emptyImg.style.display = taskList.children.length === 0 ? 'block' : 'none';
+        $emptyImg.style.display = $taskList.children.length === 0 ? 'block' : 'none';
     }
 
-    //console.log('placeholder text is:',(!taskInput.value)?'empty':taskInput.value);
 
-    const addTask =(event) =>{
+
+    //console.log('placeholder text is:',(!$taskInput.value)?'empty':$taskInput.value);
+
+    const addTask =(event)=>{
         event.preventDefault();
 
             // If you wanted to know HOW task was added:
   /*  if(event.type === 'click') {
         console.log("Added by ++++");
     } else if(event.type === 'keypress') {
-        console.log(`Added by pressing ${event.key} key`);
+        console.log(`Added by pressing ${event.key} KEY`);
     }*/
 
-        const taskText = taskInput.value;
+        const taskText = $taskInput.value;
         if(!taskText){return;}
 
         const li = document.createElement('li');
         li.innerHTML=`
-        <input type='checkbox' class='checkbox'>
+        <input type="checkbox" class="checkbox">
         <span>${taskText}</span>
         `;
 
-        //console.log("before",li.textContent);
-        li.textContent = taskText;
-        //console.log("after",li.textContent);
-        //console.log('placeholder text is:',(!taskInput.value)?'empty':taskInput.value);
 
-        taskList.appendChild(li);
-        taskInput.value = '';
+        //console.log("before",li.textContent);
+        //li.textContent = taskText;
+        //console.log("after",li.textContent);
+        //console.log('placeholder text is:',(!$taskInput.value)?'empty':$taskInput.value);
+
+        $taskList.appendChild(li);
+        $taskInput.value = '';
         toggleEmptyState();
+
     }
 
-    addTaskBtn.addEventListener('click', addTask);
+    $addTaskBtn.addEventListener('click', addTask);
 
-    taskInput.addEventListener('keypress',(e)=>{
+    $taskInput.addEventListener('keypress',(e)=>{
         if(e.key === 'Enter'){
             addTask(e);
         }
@@ -49,7 +56,11 @@ document.addEventListener('DOMContentLoaded', () => {
       );
 
 
+
+
+
 });
+
 
 /*
 
